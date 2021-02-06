c53852aa14a6f0b5007a1b94d3ccee861ab75ce6
Hunter <manuel.lara1984@gmail.com>
2021-02-06 02:16:05 +0100

    Update program

--- test-sdk/Program.cs
+++ test-sdk/Program.cs
@@ -1,63 +1,79 @@
-﻿    using System;
-    using System.Threading.Tasks;
-    using Couchbase;
-    using Microsoft.Extensions.Logging;
-    using NLog;
-    using NLog.Extensions.Logging;
+﻿using System;
+using System.Threading.Tasks;
+using Couchbase;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
 
-    namespace test_sdk
+namespace test_sdk
+{
+    class Program
     {
-        class Program
+        [Obsolete]
+        public static async Task Main(string[] args)
         {
-            [Obsolete]
-            public static async Task Main(string[] args)
+            Console.WriteLine("Hello World!");
+
+            ILoggerFactory loggerFactory = new LoggerFactory();
+
+            _ = loggerFactory.AddNLog(new NLogProviderOptions
             {
-                Console.WriteLine("Hello World!");
+                CaptureMessageTemplates = true,
+                CaptureMessageProperties = true
+            });
 
-                ILoggerFactory loggerFactory = new LoggerFactory();
+            LogManager.LoadConfiguration("Nlog.config");
 
-                _ = loggerFactory.AddNLog(new NLogProviderOptions
-                {
-                    CaptureMessageTemplates = true,
-                    CaptureMessageProperties = true
-                });
+            // connection string and credentials are passed as arguments
 
-                LogManager.LoadConfiguration("Nlog.config");
+            var options = new ClusterOptions()
+            {
+                Logging = loggerFactory,
+                UserName = args[1],
+                Password = args[2],
+                ConnectionString = args[0]
+            };
 
-                var options = new ClusterOptions()
-                {
-                    Logging = loggerFactory,
-                    UserName = "Administrator",
-                    Password = "YStv9AQU2rtHGUUr",
-                    ConnectionString = "couchbase://cb-dev.farmhub.falckrenewables.com"
-                };
+            var bucketName = "semantic";
 
-                var bucketName = "semantic";
-
-                var cluster = await Cluster.ConnectAsync(options);
+            await using (var cluster = await Cluster.ConnectAsync(options))
+            {
                 var bucket = await cluster.BucketAsync(bucketName);
                 var collection = bucket.DefaultCollection();
 
-                var result = await cluster.QueryAsync<dynamic>(
-                    $"SELECT t.* FROM `{bucketName}` t WHERE t.type=$1",
-                    options => options.Parameter("measure")
-                );
-
-                await foreach (var row in result)
+                try
                 {
-                    // each row is an instance of the Query<T> call (e.g. dynamic or custom type)
-                    var name = row.area;                        // "TECH"
-                    var address = row.semanticArtifactKey;      // NVRTR
-                    Console.WriteLine($"{name},{address}");
+                    var result = await cluster.QueryAsync<dynamic>(
+                        $"SELECT t.* FROM `{bucketName}` t WHERE t.type=$1 LIMIT 10",
+                        options => options.Parameter("measure")
+                    );
+
+                    await foreach (var row in result)
+                    {
+                        // each row is an instance of the Query<T> call (e.g. dynamic or custom type)
+                        var name = row.createdBy;                        // "TECH"
+                        var address = row.updateDate;                    // NVRTR
+                        Console.WriteLine($"{name},{address}");
+                    }
+
+                    Console.WriteLine(result.Rows.ToString());
+                
+                    await collection.UpsertAsync<string>("beer-sample-101", "logging example 101");
+
+                    var returnVal = await collection.GetAsync("beer-sample-101");
+
+                    Console.WriteLine(returnVal.ContentAs<string>());
                 }
-
-                Console.WriteLine(result.Rows.ToString());
-
-                await collection.UpsertAsync<string>("beer-sample-101", "logging example 101");
-
-                var returnVal = await collection.GetAsync("beer-sample-101");
-
-                Console.WriteLine(returnVal.ContentAs<string>());
+                catch (Exception ex)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("################## READ THIS EXCEPTION ##################");
+                    Console.WriteLine($"Exception: {ex.Message}, stack: {ex.StackTrace}");
+                    Console.WriteLine();
+                }
             }
+
+            await Task.Delay(TimeSpan.FromSeconds(30));
         }
     }
+}
